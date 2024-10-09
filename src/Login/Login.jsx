a0650902454873bc6d65177f6d37f6d3c882883d
musharaf-musemind <musharaf.hossian@musemind.agency>
2024-10-09 13:49:08 +0600

    signup and signout added.

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from '../firebase/firebase.init';
+import { signOut } from 'firebase/auth/cordova';
 
 const Login = () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
+    const [user, setUser] = useState(null);
 
     const signupHandle = ()=> {
         signInWithPopup(auth, provider)
         .then(result => {
-            const user = result.user;
-            console.log(user);
+            const loggedInUser = result.user;
+            setUser(loggedInUser);
+            // console.log(loggedInUser.displayName);
+            
             
         })
         .catch(error=> {
@@ -18,9 +22,20 @@ const Login = () => {
             
         })
     }
+
+    const signOutHandle = ()=> {
+        signOut(auth)
+        .then(result => {
+            console.log(result);
+            setUser(null);
+        })
+        .catch(error => console.log(error))
+    }
     return (
         <div>
-            <button onClick={signupHandle} className='btn button-success'> Google</button>
+            <button onClick={signupHandle} className='btn button-success'> Sign in Google</button>
+            <button onClick={signOutHandle} className='btn button-success'> Sign Out</button>
+            {user && <div>User Name: {user.displayName} </div>}
         </div>
     );
 };
